1a28c2254a3505c840335c1c169b7f14160abfc9
alzahraaltamimi <126944657+alzahraaltamimi@users.noreply.github.com>
2024-06-20 17:33:12 +0400

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,8 @@
+const btn=document.getElementById("emoji")
+const emojis=["😀","😎","🥶","🤯","🩵","😶‍🌫️","🦜","🌚","✨","🫧","⚡️","🤺","😵‍💫","🪩","🪼","🫨","🍋","💅🏼","👋🏽","🐉","🐈‍⬛","🏀","🔮","🎁","🪄","❤️‍🔥"];
+
+btn.addEventListener("mouseover",
+()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)]
+}
+)
